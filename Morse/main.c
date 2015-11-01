5ce67fc259e3e75c52aa9f583bd5dbf0be8a1268
Amin Amjadi <ma.amjadi@icloud.com>
2015-11-01 17:11:17 +0100

    Initial Commit

---
+++ Morse/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Morse
+//
+//  Created by Amin Amjadi on 11/1/15.
+//  Copyright © 2015 AIT. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
